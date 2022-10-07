4c8947ca88d0136c19a84b9d42984417385d6062
tgashwinkumar <tgashwinkumar@gmail.com>
2022-10-07 16:15:44 +0530

    Template

--- App.js
+++ App.js
@@ -20,11 +20,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-<<<<<<< HEAD
-        <Stack.Screen name="Test" component={TestScreen} />
-=======
         <Stack.Screen name="PDF" component={Pdf} />
->>>>>>> c7d324a6631464ba2d299cf85174032aedcee3d4
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Forms" component={Forms} />
         <Stack.Screen name="Timer" component={TimerScreen} />
